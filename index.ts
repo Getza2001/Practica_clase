8aa569d0522bbf54e53ef1baa7ebed2936edb208
Getza2001 <193245@ids.upchiapas.edu.mx>
2021-03-08 20:21:11 -0600

    Practica en clase

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const server = Server.instance;
-
+//getza dentro de getza-develop
 server.app.use(bodyParser.urlencoded({ extended: true}));
 server.app.use(bodyParser.json());
 
